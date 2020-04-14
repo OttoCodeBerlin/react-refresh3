50d1fcf2d9ccddf72d1c5a09c46b12b5fca96ac3
Otto Schmid <otto.schmid@Ottos-MacBook-Pro.local>
2020-04-14 17:57:09 +0200

    Finished, looks good

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import InputGroup from 'react-bootstrap/InputGroup'
 
 import FoodCard from './components/FoodCard'
 
@@ -17,13 +18,29 @@ export default class App extends Component {
     super(props)
 
     this.state = {
+      filteredFoods: foods,
       foods,
       formIsVisible: false,
+      name: '',
+      calories: '',
+      image: '',
+      search: '',
+      todayName: [],
+      todayIdx: [],
+      todayCals: [],
+      todayQty: [],
+      todayTotalCals: 0,
     }
-    console.log(this.state.foods)
 
     this.handleOpenForm = this.handleOpenForm.bind(this)
     this.handleAddFood = this.handleAddFood.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+    this.handleClearSearchForm = this.handleClearSearchForm.bind(this)
+    this.handlePlus = this.handlePlus.bind(this)
+    this.handleMinus = this.handleMinus.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   handleOpenForm = function () {
@@ -32,10 +49,114 @@ export default class App extends Component {
     })
   }
 
-  handleAddFood = function () {
-    const idx = Math.floor(Math.random() * this.state.contactsRemainder.length)
+  handleAddFood = function (e) {
+    e.preventDefault()
+    const { name, calories, image } = this.state
+    const foodObj = { name, calories, image, quantity: 0 }
+    const foodsCopy = [...this.state.foods]
+    foodsCopy.push(foodObj)
     this.setState({
-      contactsInit: [...this.state.contactsInit, this.state.contactsRemainder[idx]],
+      foods: foodsCopy,
+    })
+    this.setState({
+      name: '',
+      calories: '',
+      image: '',
+    })
+    this.handleOpenForm()
+  }
+
+  handleChange = function (e) {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSearch = function (e) {
+    this.setState({ search: e.target.value })
+  }
+
+  handleClearSearchForm = function () {
+    this.setState({ search: '' })
+  }
+
+  handlePlus = function (e, idx) {
+    e.preventDefault()
+    const foodsCopy = [...this.state.foods]
+    foodsCopy[idx].quantity++
+    this.setState({
+      foods: foodsCopy,
+    })
+  }
+
+  handleMinus = function (e, idx) {
+    e.preventDefault()
+    const foodsCopy = [...this.state.foods]
+    if (foodsCopy[idx].quantity > 0) foodsCopy[idx].quantity--
+    this.setState({
+      foods: foodsCopy,
+    })
+  }
+
+  handleAdd = function (e, idx) {
+    e.preventDefault()
+    if (this.state.foods[idx].quantity > 0) {
+      if (this.state.todayIdx.find((e) => e === idx) !== undefined) {
+        const smallIdx = this.state.todayIdx.findIndex((e) => e === idx)
+        const qtyCopy = [...this.state.todayQty]
+        qtyCopy[smallIdx] += this.state.foods[idx].quantity
+        const calsCopy = [...this.state.todayCals]
+        calsCopy[smallIdx] += this.state.foods[idx].calories * this.state.foods[idx].quantity
+        const totalCals = this.state.todayTotalCals + this.state.foods[idx].calories * this.state.foods[idx].quantity
+        const foodsCopy = [...this.state.foods]
+        foodsCopy[idx].quantity = 0
+        this.setState({
+          todayCals: calsCopy,
+          todayQty: qtyCopy,
+          todayTotalCals: totalCals,
+          foods: foodsCopy,
+        })
+      } else {
+        const nameCopy = [...this.state.todayName]
+        nameCopy.push(this.state.foods[idx].name)
+        const idxCopy = [...this.state.todayIdx]
+        idxCopy.push(idx)
+        const qtyCopy = [...this.state.todayQty]
+        qtyCopy.push(this.state.foods[idx].quantity)
+        const calsCopy = [...this.state.todayCals]
+        calsCopy.push(this.state.foods[idx].calories * this.state.foods[idx].quantity)
+        const totalCals = this.state.todayTotalCals + this.state.foods[idx].calories * this.state.foods[idx].quantity
+        const foodsCopy = [...this.state.foods]
+        foodsCopy[idx].quantity = 0
+        this.setState({
+          todayName: nameCopy,
+          todayCals: calsCopy,
+          todayQty: qtyCopy,
+          todayIdx: idxCopy,
+          todayTotalCals: totalCals,
+          foods: foodsCopy,
+        })
+      }
+    }
+  }
+
+  handleDelete = function (e, idx) {
+    e.preventDefault()
+    const nameCopy = [...this.state.todayName]
+    nameCopy.splice(idx, 1)
+    const idxCopy = [...this.state.todayIdx]
+    idxCopy.splice(idx, 1)
+    const qtyCopy = [...this.state.todayQty]
+    qtyCopy.splice(idx, 1)
+    const calsCopy = [...this.state.todayCals]
+    const calsToDelete = calsCopy.splice(idx, 1)
+    const totalCals = this.state.todayTotalCals - calsToDelete
+
+    this.setState({
+      todayName: nameCopy,
+      todayCals: calsCopy,
+      todayQty: qtyCopy,
+      todayIdx: idxCopy,
+      todayTotalCals: totalCals,
     })
   }
 
@@ -49,13 +170,19 @@ export default class App extends Component {
             Add Your Food
           </Button>
           {this.state.formIsVisible && (
-            <Form>
+            <Form onSubmit={this.handleAddFood}>
               <Form.Group as={Row} controlId="formHorizontalName">
                 <Form.Label column sm={4}>
                   Name
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Control type="text" placeholder="e.g. Schweinebraten" />
+                  <Form.Control
+                    name="name"
+                    type="text"
+                    placeholder="e.g. Schweinebraten"
+                    value={this.state.foodName}
+                    onChange={(e) => this.handleChange(e)}
+                  />
                 </Col>
               </Form.Group>
               <Form.Group as={Row} controlId="formHorizontalCalories">
@@ -63,7 +190,13 @@ export default class App extends Component {
                   Calories
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Control type="number" placeholder="e.g. 800" />
+                  <Form.Control
+                    name="calories"
+                    type="number"
+                    placeholder="e.g. 800"
+                    value={this.state.foodCals}
+                    onChange={(e) => this.handleChange(e)}
+                  />
                 </Col>
               </Form.Group>
               <Form.Group as={Row} controlId="formHorizontalImg">
@@ -71,10 +204,16 @@ export default class App extends Component {
                   Image URL
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Control type="text" placeholder="www.xxx.com/xyz.jpg" />
+                  <Form.Control
+                    name="image"
+                    type="text"
+                    placeholder="www.xxx.com/xyz.jpg"
+                    value={this.state.foodImgUrl}
+                    onChange={(e) => this.handleChange(e)}
+                  />
                 </Col>
               </Form.Group>
-              <Button className="mb-3 mr-2" variant="outline-primary" onClick={this.handleOpenForm}>
+              <Button type="submit" className="mb-3 mr-2" variant="outline-primary">
                 Add
               </Button>
               <Button className="mb-3 mr-2" variant="outline-secondary" onClick={this.handleOpenForm}>
@@ -86,10 +225,71 @@ export default class App extends Component {
           <hr />
         </Container>
         <Container>
-          <CardDeck>
-            {this.state.foods.map((el, idx) => (
-              <FoodCard key={idx} name={el.name} cals={el.calories} qty={el.quantity} img={el.image} />
-            ))}
+          <InputGroup>
+            <Form.Control
+              name="search"
+              type="text"
+              placeholder="Search Food ..."
+              value={this.state.search}
+              onChange={(e) => this.handleSearch(e)}
+            />
+            <InputGroup.Append onClick={this.handleClearSearchForm}>
+              <InputGroup.Text id="inputGroupPrepend">X</InputGroup.Text>
+            </InputGroup.Append>
+          </InputGroup>
+          <hr />
+        </Container>
+        <Container>
+          {this.state.todayName.length > 0 && (
+            <div>
+              <h3>Today's Food</h3>
+              <br />
+              {this.state.todayName.map((el, idx) => (
+                <Row key={idx}>
+                  <Col>
+                    <strong>
+                      <p>
+                        {el} * {this.state.todayQty[idx]}
+                      </p>
+                    </strong>
+                  </Col>
+                  <Col>
+                    <p>{this.state.todayCals[idx]} Calories</p>
+                  </Col>
+                  <Col>
+                    <p
+                      style={{ color: 'lightgrey', textDecoration: 'underline', cursor: 'pointer' }}
+                      onClick={(e) => this.handleDelete(e, idx)}
+                    >
+                      Delete
+                    </p>
+                  </Col>
+                </Row>
+              ))}
+              <hr />
+              <h6>Total {this.state.todayTotalCals} Calories</h6>
+              <hr />
+            </div>
+          )}
+        </Container>
+
+        <Container>
+          <CardDeck className="justify-content-md-center">
+            {this.state.foods
+              .filter((e) => e.name.toUpperCase().includes(this.state.search.toUpperCase()))
+              .map((el, idx) => (
+                <FoodCard
+                  key={idx}
+                  index={idx}
+                  name={el.name}
+                  cals={el.calories}
+                  qty={el.quantity}
+                  img={el.image}
+                  handlePlus={this.handlePlus}
+                  handleMinus={this.handleMinus}
+                  handleAdd={this.handleAdd}
+                />
+              ))}
           </CardDeck>
         </Container>
       </div>
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -18,13 +18,20 @@ export default function FoodCard(props) {
 
           <Row>
             <Col>
-              <Button variant="primary">+</Button>
+              <Button variant="primary-link" onClick={(e) => props.handleMinus(e,props.index)}>-</Button>
             </Col>
             <Col>
               <Card.Text className="mt-2">
                 <strong>{props.qty}</strong>
               </Card.Text>
             </Col>
+            <Col>
+              <Button variant="primary-link" onClick={(e) => props.handlePlus(e,props.index)}>+</Button>
+            </Col>
+            <Col>
+              <Button variant="primary mt-2" onClick={(e) => props.handleAdd(e,props.index)}>Add</Button>
+            </Col>
+            
           </Row>
         </Card.Body>
       </Card>
